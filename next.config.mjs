53af41e31c2b94288b1c3d7fbef343aed14b29cc
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-03-25 13:08:51 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,39 @@
-import createNextIntlPlugin from "next-intl/plugin";
-import withPWA from "next-pwa";
+// import createNextIntlPlugin from "next-intl/plugin";
+// import withPWA from "next-pwa";
+
+// const withNextIntl = createNextIntlPlugin();
+
+// const nextConfig = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === "development",
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//     ],
+//   },
+// });
+
+// export default withNextIntl(nextConfig);
+
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
-
-const nextConfig = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-});
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
+    },
+};
 
 export default withNextIntl(nextConfig);
